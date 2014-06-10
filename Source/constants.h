111939cc774360a40bf250fb1ac6411f69d6532f
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2014-06-09 22:46:31 -0400

    added new - path demo completed

---
+++ Source/constants.h
@@ -0,0 +1,14 @@
+//
+//  constants.h
+//  SpiderRock
+//
+//  Created by Ziyuan Liu on 6/9/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#ifndef SpiderRock_constants_h
+#define SpiderRock_constants_h
+
+
+
+#endif
