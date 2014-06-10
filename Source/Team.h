49164233b821dd581214c487439ab57b656d2948
ziyuanliu <ziyuan.liu.16@dartmouth.edu>
2014-06-10 11:03:32 -0400

    comit

---
+++ Source/Team.h
@@ -0,0 +1,13 @@
+//
+//  Team.h
+//  SpiderRock
+//
+//  Created by Ziyuan Liu on 6/9/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Team : NSObject
+
+@end
---
+++ Source/Team.m
@@ -0,0 +1,13 @@
+//
+//  Team.m
+//  SpiderRock
+//
+//  Created by Ziyuan Liu on 6/9/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Team.h"
+
+@implementation Team
+
+@end
